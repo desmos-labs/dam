8d186eca9f9a15979700a5dc953b3b9d72a51521
Manuel <manuel.turetta94@gmail.com>
2021-06-30 16:52:32 +0200

    Added generated files

---
+++ lib/di/di.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'di.dart';
+
+// **************************************************************************
+// KiwiInjectorGenerator
+// **************************************************************************
+
+class _$Injector extends Injector {
+  @override
+  void configure() {
+    final KiwiContainer container = KiwiContainer();
+    container.registerSingleton((c) => WalletSource());
+  }
+}
